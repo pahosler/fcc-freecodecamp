7e73f5f6988d42744313d8b1f3e3a7e130c5690b
Paul Hosler <pahosler@gmail.com>
2016-07-24 02:05:30 -0500

    wip

--- www/wikiviewer/js/wikiviewer.js
+++ www/wikiviewer/js/wikiviewer.js
@@ -1,49 +1,59 @@
-$(document).ready(function() {
-  //  function() {
-        var results = {
-            // things: ['thing1', 'thing2'];
-            init: function() {
-                //called from outside thing
-                this.cacheDom();
-                this.bindEvents();
-                this.addResults();
-            },
-            cacheDom: function() {
-                //example only
-                this.$el = $("#search-module");
-                this.$button = this.$el.find('button');
-                this.$input = this.$el.find('input');
-                this.$results = this.$el.find('#results');
-                this.template = this.$el.find('#searchwiki').html();
-            },
-            bindEvents: function() {
-          //      this.$button.on('click', this.addThing.bind(this));
-                this.$input.keyup(this.addResults.bind(this));
-            },
-            render: function() {
-              var data = [...arguments];
-              console.log(data[0]);
-              if(data[0] === 'undefined') {
-                this.render();
-              }
-                this.$results.empty();
-                $.each(data[0].query.search, function(i, item) {
-                    this.$results.html(Mustache.render(this.template, item));
-                });
-            },
-            addResults: function() {
-                var q = this.$input.val();
-                $.getJSON("http://en.wikipedia.org/w/api.php?callback=?", {
-                        srsearch: q,
-                        action: "query",
-                        list: "search",
-                        format: "json"
-                    }),
-                    this.render();
-            }
-        };
-        results.init();
-  //  }();
+(function() {
+    var results = {
+        data: '',
+        init: function() {
+            //called from outside thing
+            console.log("INIT!");
+            this.cacheDom();
+            this.bindEvents();
+            //this.addResults();
+        },
+        cacheDom: function() {
+            //example only
+            this.$el = $("#search-module");
+            this.$button = this.$el.find('#search');
+            this.$input = this.$el.find('input');
+            this.$results = this.$el.find('#results');
+            this.template = this.$el.find('#search-template').html();
+            console.log("DOM cached!");
+        },
+        bindEvents: function() {
+            //      this.$button.on('click', this.addThing.bind(this));
+            console.log("Waiting to search!");
+            this.$button.on('click',this.doit("Hello World!"));//.bind(this));
+        },
+        render: function(data) {
+            console.log("HELLO WORLD!");
+            console.log(data);
+            // if (data === '') {
+            //     return;
+            // }
+            // this.$results.empty();
+            // $.each(data.query.search, function(i, item) {
+            //     this.$results.html(Mustache.render(this.template, item));
+            // });
+            this.$results.html(Mustache.render(this.template,data));
+        },
+        addResults: function() {
+            console.log("Calling addResults!");
+            var q = this.$input.val().bind(this);
+            console.log(q);
+            $.getJSON("http://en.wikipedia.org/w/api.php?callback=?", {
+                    srsearch: q,
+                    action: "query",
+                    list: "search",
+                    format: "json"
+                }),
+                this.render(this.data);
+        },
+        doit: function(str) {
+          console.log("Full of searchyness!!!",str);
+          console.log(this.$input.val());
+          this.$input.val('');
+          this.render(this.$input.val());
+        }
+    };
+    results.init();
 
     // $("#searchterm").keyup(function(e) {
     //     var q = $("#searchterm").val();
@@ -61,4 +71,4 @@ $(document).ready(function() {
     //             });
     //         });
     // });
-});
+})()
