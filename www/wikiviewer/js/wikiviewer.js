7c1fa831e0ea5219d2a1ced31f465efbfe2c7d32
Paul Hosler <pahosler@gmail.com>
2016-07-25 03:08:38 -0500

    wip

--- www/wikiviewer/js/wikiviewer.js
+++ www/wikiviewer/js/wikiviewer.js
@@ -1,74 +1,56 @@
-(function() {
+$(document).ready(function() {
     var results = {
-        data: '',
+        info: {
+          uri: [],
+          wiki: [],
+          title: []
+        },
         init: function() {
-            //called from outside thing
             console.log("INIT!");
             this.cacheDom();
             this.bindEvents();
-            //this.addResults();
         },
         cacheDom: function() {
-            //example only
             this.$el = $("#search-module");
-            this.$button = this.$el.find('#search');
+            this.$button = this.$el.find('button');
             this.$input = this.$el.find('input');
-            this.$results = this.$el.find('#results');
+            this.$ul = this.$el.find('#wikis');
             this.template = this.$el.find('#search-template').html();
-            console.log("DOM cached!");
         },
         bindEvents: function() {
-            //      this.$button.on('click', this.addThing.bind(this));
-            console.log("Waiting to search!");
-            this.$button.on('click',this.doit("Hello World!"));//.bind(this));
+            this.$button.on('click', this.addResults.bind(this));
         },
-        render: function(data) {
-            console.log("HELLO WORLD!");
-            console.log(data);
-            // if (data === '') {
-            //     return;
-            // }
-            // this.$results.empty();
-            // $.each(data.query.search, function(i, item) {
-            //     this.$results.html(Mustache.render(this.template, item));
-            // });
-            this.$results.html(Mustache.render(this.template,data));
+        render: function() {
+            var data = {
+                uri: results.info.uri,
+              //  title: results.info.title,
+              //  wiki: results.info.wiki,
+            };
+            console.log("time to render...",data);
+            for (var i = 0; i < results.info.uri.length; ++i) {
+                this.$ul.html(Mustache.render(this.template, data));
+            }
         },
         addResults: function() {
-            console.log("Calling addResults!");
-            var q = this.$input.val().bind(this);
-            console.log(q);
+            var q = this.$input.val();
+            console.log("entered addResults...", q);
+
             $.getJSON("http://en.wikipedia.org/w/api.php?callback=?", {
                     srsearch: q,
                     action: "query",
                     list: "search",
                     format: "json"
-                }),
-                this.render(this.data);
-        },
-        doit: function(str) {
-          console.log("Full of searchyness!!!",str);
-          console.log(this.$input.val());
-          this.$input.val('');
-          this.render(this.$input.val());
+                },
+                function(data) {
+                    console.log("calling render", q);
+                    $.each(data.query.search, function(i, item) {
+                        results.info.uri.push(encodeURIComponent(item.title));
+                        results.info.wiki.push(item.snippet);
+                        results.info.title.push(item.title);
+                    });
+                });
+                this.render();
         }
     };
     results.init();
-
-    // $("#searchterm").keyup(function(e) {
-    //     var q = $("#searchterm").val();
-    //     $.getJSON("http://en.wikipedia.org/w/api.php?callback=?", {
-    //             srsearch: q,
-    //             action: "query",
-    //             list: "search",
-    //             format: "json"
-    //         },
-    //         function(data) {
-    //             $("#results").empty();
-    //             $("#results").append("<p>Results for <b>" + q + "</b></p>");
-    //             $.each(data.query.search, function(i, item) {
-    //                 $("#results").append("<div><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + "</a><br>" + item.snippet + "<br><br></div>");
-    //             });
-    //         });
-    // });
-})()
+})
