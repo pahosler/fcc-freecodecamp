a7d095136418bd5a4257b1156b3872fe7431edee
Paul Hosler <pahosler@gmail.com>
2016-07-30 13:30:48 -0500

    wip

--- www/twitch/js/twitch.js
+++ www/twitch/js/twitch.js
@@ -36,15 +36,15 @@ $(document).ready(function() {
             this.$buttonFeatured.on('click', this.getFeatured.bind(this));
         },
         render: function() {
-                this.info.data.push({
-                    "url": this.info.uri,
-                    "title": this.info.userName,
-                    "image": this.info.userLogo,
-                    "status": this.info.userStatus,
-                    "broadcast": this.info.userBroadcast,
-                    "onoffL" : this.info.onoffL,
-                    "onoffS" : this.info.onoffS
-                  });
+            this.info.data.push({
+                "url": this.info.uri,
+                "title": this.info.userName,
+                "image": this.info.userLogo,
+                "status": this.info.userStatus,
+                "broadcast": this.info.userBroadcast,
+                "onoffL": this.info.onoffL,
+                "onoffS": this.info.onoffS
+            });
         },
         getData: function() {
             for (var stream = 0; stream < this.stream.length; stream++) {
@@ -52,28 +52,30 @@ $(document).ready(function() {
                     if (data.stream === null) {
                         twitch.getOfflineInfo(data._links.channel);
                     } else {
-                            twitch.info.userLogo=data.stream.channel.logo;
-                            twitch.info.uri=data.stream.channel.url;
-                            twitch.info.userName=data.stream.channel.display_name;
-                            twitch.info.userStatus=data.stream.channel.status;
-                            twitch.info.userBroadcast="On Line";
-                            twitch.info.onoffS="onsmall";
-                            twitch.info.onoffL="onlarge";
+                        twitch.info.userLogo = data.stream.channel.logo;
+                        twitch.info.uri = data.stream.channel.url;
+                        twitch.info.userName = data.stream.channel.display_name;
+                        twitch.info.userStatus = data.stream.channel.status;
+                        twitch.info.userBroadcast = "On Line";
+                        twitch.info.onoffS = "onsmall";
+                        twitch.info.onoffL = "onlarge";
                         twitch.render();
                     }
                 });
             }
         },
         getFeatured: function() {
-          this.info.data=[];
+            this.info.data = [];
             $.getJSON('https://api.twitch.tv/kraken/streams/featured', function(data) {
                 for (var i = 0; i < data.featured.length; i++) {
-                    twitch.info.userName=data.featured[i].stream.channel.display_name;
-                    twitch.info.uri=data.featured[i].stream.channel.url;
-                    twitch.info.userLogo=data.featured[i].stream.channel.logo;
-                    twitch.info.userStatus=data.featured[i].stream.channel.status;
-                    twitch.info.onoffS="onsmall";
-                    twitch.info.onoffL="onlarge";
+                    twitch.info.userName = data.featured[i].stream.channel.display_name;
+                    twitch.info.uri = data.featured[i].stream.channel.url;
+                    twitch.info.userLogo = data.featured[i].stream.channel.logo;
+                    twitch.info.userStatus = data.featured[i].stream.channel.status;
+                    twitch.info.onoffS = "onsmall";
+                    twitch.info.onoffL = "onlarge";
+                    console.log(i);
+
                     twitch.render();
                 }
             });
@@ -85,39 +87,44 @@ $(document).ready(function() {
         },
         getOfflineInfo: function(channels) {
             $.getJSON(channels + '?callback=?', function(data) {
-                    twitch.info.userLogo=data.logo;
-                    twitch.info.uri=data.url;
-                    twitch.info.userName=data.display_name;
-                    twitch.info.userStatus=data.status;
-                    twitch.info.userBroadcast="Off Line";
-                    twitch.info.onoffS="offsmall";
-                    twitch.info.onoffL="offlarge";
-                    twitch.render();
+                twitch.info.userLogo = data.logo;
+                twitch.info.uri = data.url;
+                twitch.info.userName = data.display_name;
+                twitch.info.userStatus = data.status;
+                twitch.info.userBroadcast = "Off Line";
+                twitch.info.onoffS = "offsmall";
+                twitch.info.onoffL = "offlarge";
+                twitch.render();
 
             });
         },
         twitchData: function() {
-          return this.info.data;
+            return this.info.data;
         }
     }
 
     twitch.init();
-
-    Vue.filter('searchFor', function (value, searchString) {
+    var MyComponent = Vue.extend({
+      featured: function() {
+              twitch.getFeatured();
+          }
+    });
+    Vue.component('my-component', MyComponent);
+    Vue.filter('searchFor', function(value, searchString) {
 
         // The first parameter to this function is the data that is to be filtered.
         // The second is the string we will be searching for.
 
         var result = [];
 
-        if(!searchString){
+        if (!searchString) {
             return value;
         }
 
         searchString = searchString.trim().toLowerCase();
 
-        result = value.filter(function(item){
-            if(item.title.toLowerCase().indexOf(searchString) !== -1){
+        result = value.filter(function(item) {
+            if (item.title.toLowerCase().indexOf(searchString) !== -1) {
                 return item;
             }
         })
@@ -130,17 +137,13 @@ $(document).ready(function() {
     var layout = new Vue({
         el: '#main',
         data: {
-          layout: 'grid',
+            layout: 'grid',
             searchString: "",
 
             // The data model. These items would normally be requested via AJAX,
             // but are hardcoded here for simplicity.
 
             articles: twitch.twitchData()
-        },
-        getFeatured:function () {
-          twitch.getFeatured();
-          // return false;
         }
     });
 
