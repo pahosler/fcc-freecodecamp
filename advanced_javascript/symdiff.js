cfc149e87c46ddd47096a079fd0de710c0d3a90e
Paul Hosler <pahosler@gmail.com>
2016-08-06 14:58:50 -0500

    fcc

--- advanced_javascript/symdiff.js
+++ advanced_javascript/symdiff.js
@@ -14,41 +14,63 @@ function sym(args) {
     var diff = [];
     var symdiffs = [];
 
-      function symdiff(a,b){
-        var temp = [];
+    function symdiff(a, b) {
         var values = a.concat(b);
-        console.log("this is v",values);
-        for(var i = values.length-1; i >0;--i){
-          var cut = values.splice(i,1);
-          console.log("this is values after splice",values);
-          console.log("this is cut",cut);
-          if(values.indexOf(cut,0) === -1 && temp.indexOf(cut) === -1){
-            temp.push(cut[0]);
+        var array =[];
+        // console.log("this is v", values);
+        for (var i = 0; i < values.length; ++i) {
+            var temp = [...values];
+            var indicies = [];
+            var idx = values.indexOf(values[i]);
+            while (idx != -1) {
+                indicies.push(idx);
+                idx = values.indexOf(values[i], idx + 1)
+            }
+            if(b === undefined){
+              if (indicies.length === 1 || indicies.length >=2) {
+                  var cut =(temp.splice(indicies[0], 1));
+                  if(cut[0] !== undefined && array.indexOf(cut[0]) == -1){
+                    array.push(cut[0]);
+                  }
+              }
+            }else {
+            if (indicies.length === 1) {
+                var cut =(temp.splice(indicies[0], 1));
+                if(cut[0] !== undefined && array.indexOf(cut[0]) == -1){
+                  array.push(cut[0]);
+                }
+            }
           }
         }
-        console.log("this is temp",temp);
-        return [...temp];
-      }
+        // console.log("this is array", array);
+        return [...array].sort();
+    }
 
-    arr.reduce(function(a,sym,idx,array){
-      // console.log(array);
-      if (a !== undefined){
-        diff = symdiff(a.sort(),sym.sort());
-      }else {
-        diff = symdiff(diff.sort(),sym.sort());
-      }
-      console.log("new diff",diff);
+    arr.reduce(function(a, sym, idx, array) {
+        // console.log(array);
+        if (a !== undefined) {
+            diff = symdiff(a.sort());
+            diff = symdiff(diff.sort(), sym.sort());
+        } else {
+          var diffs = symdiff(sym.sort());
+          // console.log("sym/diffs",sym,diffs);
+          diff = symdiff(diff.sort(), diffs.sort());
+          // console.log("diff",diff);
+        }
+        // console.log("new diff", diff);
+
     });
+    return diff;
 
 
 }
 //sym([5, 2, 1, 4], [1, 2, 3]);
-console.log("I am...",sym([1, 2, 3], [5, 2, 1, 4])); //should return [3, 4, 5].
-console.log("I am...", sym([1, 2, 5], [2, 3, 5], [3, 4, 5])); // should return [1, 4, 5].
-// console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5])); // should contain only three elements.
-// console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])); // should return [1, 4, 5].
-// console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])); // should contain only three elements.
-// console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])); // should return [2, 3, 4, 6, 7].
-// console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])); // should contain only five elements.
-// console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])); // should return [1, 2, 4, 5, 6, 7, 8, 9].
-// console.log("testing",sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])); // should contain only eight elements.
+console.log("I am...", sym([1, 2, 3], [5, 2, 1, 4])); //should return [3, 4, 5].
+console.log("I am...", sym([1,1,1,1, 2, 5], [2, 3, 5], [3, 4, 5])); // should return [1, 4, 5].
+console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5])); // should contain only three elements.
+console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])); // should return [1, 4, 5].
+console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])); // should contain only three elements.
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])); // should return [2, 3, 4, 6, 7].
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])); // should contain only five elements.
+console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])); // should return [1, 2, 4, 5, 6, 7, 8, 9].
+console.log("testing",sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])); // should contain only eight elements.
