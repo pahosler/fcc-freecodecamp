a8ad3f16f61c3551fbf71b8ee536e94e1eba95b4
Paul Hosler <pahosler@gmail.com>
2016-08-08 14:55:41 -0500

    fcc

--- advanced_javascript/exact_change.js
+++ advanced_javascript/exact_change.js
@@ -12,12 +12,46 @@
 // Otherwise, return change in coin and bills, sorted in highest to lowest order.
 
 function checkCashRegister(price, cash, cid) {
+    var denomination = {};
     var change;
-    var totalInDrawer = cid.reduce(function(a,b,index){
-      return a[index,1]+b[index,1];
-    });
-    console.log("total cash in drawer",totalInDrawer);
-    // Here is your change, ma'am.
+    var totalInDrawer = 0;
+    var change = cash - price;
+
+    function makeChange(change){
+      switch (change){
+        case 20-100: {
+          return change ="20s";
+        }
+        case  10-19:{
+          return change ="10s"
+        }
+        case  1-9:{
+          return change ="1s"
+        }
+        case .01-.99:{
+          return change = "cents";
+        }
+      }
+      return change;
+    }
+
+    for( var currency of cid){
+      totalInDrawer += (currency[currency,1]);
+      denomination[currency[currency,0]] = currency[currency,1];
+    }
+
+    // javascript SUCKS at adding simple floating point numbers
+    // as a result you have to use this hack to get a proper total!
+    totalInDrawer = (totalInDrawer*100)/100;
+    if(change > totalInDrawer){
+      change = "Insufficient Funds";
+      return change;
+    } else if(change === totalInDrawer){
+      change = "Closed";
+      return change;
+    }else{
+      return makeChange(change);
+    }
     return change;
 }
 
@@ -32,7 +66,7 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60.00],
 // ["ONE HUNDRED", 100.00]]
 
-checkCashRegister(19.50, 20.00, [
+console.log("transaction 1",checkCashRegister(19.50, 20.00, [
     ["PENNY", 1.01],
     ["NICKEL", 2.05],
     ["DIME", 3.10],
@@ -42,90 +76,90 @@ checkCashRegister(19.50, 20.00, [
     ["TEN", 20.00],
     ["TWENTY", 60.00],
     ["ONE HUNDRED", 100.00]
-]);
-// checkCashRegister(19.50, 20.00, [
-//     ["PENNY", 0.01],
-//     ["NICKEL", 0],
-//     ["DIME", 0],
-//     ["QUARTER", 0],
-//     ["ONE", 0],
-//     ["FIVE", 0],
-//     ["TEN", 0],
-//     ["TWENTY", 0],
-//     ["ONE HUNDRED", 0]
-// ]); // should return a string.
-// checkCashRegister(19.50, 20.00, [
-//     ["PENNY", 0.50],
-//     ["NICKEL", 0],
-//     ["DIME", 0],
-//     ["QUARTER", 0],
-//     ["ONE", 0],
-//     ["FIVE", 0],
-//     ["TEN", 0],
-//     ["TWENTY", 0],
-//     ["ONE HUNDRED", 0]
-// ]); // should return a string.
-// checkCashRegister(19.50, 20.00, [
-//     ["PENNY", 1.01],
-//     ["NICKEL", 2.05],
-//     ["DIME", 3.10],
-//     ["QUARTER", 4.25],
-//     ["ONE", 90.00],
-//     ["FIVE", 55.00],
-//     ["TEN", 20.00],
+]));
+console.log("transaction 2",checkCashRegister(19.50, 20.00, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+])); // should return a string.
+console.log("transaction 3",checkCashRegister(19.50, 20.00, [
+    ["PENNY", 0.50],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+])); // should return a string.
+console.log("transaction 4",checkCashRegister(19.50, 20.00, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.10],
+    ["QUARTER", 4.25],
+    ["ONE", 90.00],
+    ["FIVE", 55.00],
+    ["TEN", 20.00],
+    ["TWENTY", 60.00],
+    ["ONE HUNDRED", 100.00]
+])); // should return [["QUARTER", 0.50]].
+console.log("transaction 5",checkCashRegister(3.26, 100.00, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.10],
+    ["QUARTER", 4.25],
+    ["ONE", 90.00],
+    ["FIVE", 55.00],
+    ["TEN", 20.00],
+    ["TWENTY", 60.00],
+    ["ONE HUNDRED", 100.00]
+]));
+// should return [
 //     ["TWENTY", 60.00],
-//     ["ONE HUNDRED", 100.00]
-// ]); // should return [["QUARTER", 0.50]].
-// checkCashRegister(3.26, 100.00, [
-//     ["PENNY", 1.01],
-//     ["NICKEL", 2.05],
-//     ["DIME", 3.10],
-//     ["QUARTER", 4.25],
-//     ["ONE", 90.00],
-//     ["FIVE", 55.00],
 //     ["TEN", 20.00],
-//     ["TWENTY", 60.00],
-//     ["ONE HUNDRED", 100.00]
-// ]);
-// // should return [
-// //     ["TWENTY", 60.00],
-// //     ["TEN", 20.00],
-// //     ["FIVE", 15.00],
-// //     ["ONE", 1.00],
-// //     ["QUARTER", 0.50],
-// //     ["DIME", 0.20],
-// //     ["PENNY", 0.04]
-// // ].
-// checkCashRegister(19.50, 20.00, [
-//     ["PENNY", 0.01],
-//     ["NICKEL", 0],
-//     ["DIME", 0],
-//     ["QUARTER", 0],
-//     ["ONE", 0],
-//     ["FIVE", 0],
-//     ["TEN", 0],
-//     ["TWENTY", 0],
-//     ["ONE HUNDRED", 0]
-// ]); //  should return "Insufficient Funds".
-// checkCashRegister(19.50, 20.00, [
-//     ["PENNY", 0.01],
-//     ["NICKEL", 0],
-//     ["DIME", 0],
-//     ["QUARTER", 0],
+//     ["FIVE", 15.00],
 //     ["ONE", 1.00],
-//     ["FIVE", 0],
-//     ["TEN", 0],
-//     ["TWENTY", 0],
-//     ["ONE HUNDRED", 0]
-// ]); //  should return "Insufficient Funds".
-// checkCashRegister(19.50, 20.00, [
-//     ["PENNY", 0.50],
-//     ["NICKEL", 0],
-//     ["DIME", 0],
-//     ["QUARTER", 0],
-//     ["ONE", 0],
-//     ["FIVE", 0],
-//     ["TEN", 0],
-//     ["TWENTY", 0],
-//     ["ONE HUNDRED", 0]
-// ]); //  should return "Closed".
+//     ["QUARTER", 0.50],
+//     ["DIME", 0.20],
+//     ["PENNY", 0.04]
+// ].
+console.log("transaction 6",checkCashRegister(19.50, 20.00, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+])); //  should return "Insufficient Funds".
+console.log("transaction 7",checkCashRegister(19.50, 20.00, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 1.00],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+])); //  should return "Insufficient Funds".
+console.log("transaction 8",checkCashRegister(19.50, 20.00, [
+    ["PENNY", 0.50],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+])); //  should return "Closed".
--- advanced_javascript/symdiff.js
+++ advanced_javascript/symdiff.js
@@ -9,62 +9,63 @@
 // you should get the set with elements which are in either of the two sets
 // but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
 
-function sym(args) {
-    var arr = [...arguments];
-    var diff = [];
-    var symdiffs = [];
-
-    function symdiff(a, b) {
-        var values = a.concat(b);
-        var array =[];
-        // console.log("this is v", values);
-        for (var i = 0; i < values.length; ++i) {
-            var temp = [...values];
-            var indicies = [];
-            var idx = values.indexOf(values[i]);
-            while (idx != -1) {
-                indicies.push(idx);
-                idx = values.indexOf(values[i], idx + 1)
-            }
-            if(b === undefined){
-              if (indicies.length === 1 || indicies.length >=2) {
-                  var cut =(temp.splice(indicies[0], 1));
-                  if(cut[0] !== undefined && array.indexOf(cut[0]) == -1){
-                    array.push(cut[0]);
-                  }
-              }
-            }else {
-            if (indicies.length === 1) {
-                var cut =(temp.splice(indicies[0], 1));
-                if(cut[0] !== undefined && array.indexOf(cut[0]) == -1){
-                  array.push(cut[0]);
-                }
-            }
-          }
-        }
-        // console.log("this is array", array);
-        return [...array].sort();
-    }
-
-    arr.reduce(function(a, sym, idx, array) {
-        // console.log(array);
-        if (a !== undefined) {
-            diff = symdiff(a.sort());
-            diff = symdiff(diff.sort(), sym.sort());
-        } else {
-          var diffs = symdiff(sym.sort());
-          // console.log("sym/diffs",sym,diffs);
-          diff = symdiff(diff.sort(), diffs.sort());
-          // console.log("diff",diff);
-        }
-        // console.log("new diff", diff);
-
-    });
-    return diff;
-
-
-}
+// function sym(args) {
+//     var arr = [...arguments];
+//     var diff = [];
+//     var symdiffs = [];
+//
+//     function symdiff(a, b) {
+//         var values = a.concat(b);
+//         var array =[];
+//         // console.log("this is v", values);
+//         for (var i = 0; i < values.length; ++i) {
+//             var temp = [...values];
+//             var indicies = [];
+//             var idx = values.indexOf(values[i]);
+//             while (idx != -1) {
+//                 indicies.push(idx);
+//                 idx = values.indexOf(values[i], idx + 1)
+//             }
+//             if(b === undefined){
+//               if (indicies.length === 1 || indicies.length >=2) {
+//                   var cut =(temp.splice(indicies[0], 1));
+//                   if(cut[0] !== undefined && array.indexOf(cut[0]) == -1){
+//                     array.push(cut[0]);
+//                   }
+//               }
+//             }else {
+//             if (indicies.length === 1) {
+//                 var cut =(temp.splice(indicies[0], 1));
+//                 if(cut[0] !== undefined && array.indexOf(cut[0]) == -1){
+//                   array.push(cut[0]);
+//                 }
+//             }
+//           }
+//         }
+//         // console.log("this is array", array);
+//         return [...array].sort();
+//     }
+//
+//     arr.reduce(function(a, sym, idx, array) {
+//         // console.log(array);
+//         if (a !== undefined) {
+//             diff = symdiff(a.sort());
+//             diff = symdiff(diff.sort(), sym.sort());
+//         } else {
+//           var diffs = symdiff(sym.sort());
+//           // console.log("sym/diffs",sym,diffs);
+//           diff = symdiff(diff.sort(), diffs.sort());
+//           // console.log("diff",diff);
+//         }
+//         // console.log("new diff", diff);
+//
+//     });
+//     return diff;
+//
+//
+// }
 //sym([5, 2, 1, 4], [1, 2, 3]);
+
 console.log("I am...", sym([1, 2, 3], [5, 2, 1, 4])); //should return [3, 4, 5].
 console.log("I am...", sym([1,1,1,1, 2, 5], [2, 3, 5], [3, 4, 5])); // should return [1, 4, 5].
 console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5])); // should contain only three elements.
@@ -74,3 +75,16 @@ console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])); // sho
 console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])); // should contain only five elements.
 console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])); // should return [1, 2, 4, 5, 6, 7, 8, 9].
 console.log("testing",sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])); // should contain only eight elements.
+
+
+// sweet ass solution from gitter chat...
+function sym(args) {
+
+  return Array.from(arguments).reduce(function(arr1,arr2){
+    return arr1.concat(arr2).filter(function(a,b,c){
+      return arr1.indexOf(a) === -1 && b === c.indexOf(a) ||
+             arr2.indexOf(a) === -1 && b === c.indexOf(a);});
+  }).sort();
+}
+
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]);
