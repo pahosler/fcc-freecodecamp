15b22b46976db8b7b0220ab64d901d2977b95177
Paul Hosler <pahosler@gmail.com>
2016-08-16 00:10:54 -0500

    free code camp

--- basic_javascript/palindrome.js
+++ basic_javascript/palindrome.js
@@ -1,27 +1,38 @@
+// Check for Palindromes
+// Return true if the given string is a palindrome. Otherwise, return false.
+//
+// A palindrome is a word or sentence that's spelled the same way both forward
+// and backward, ignoring punctuation, case, and spacing.
+//
+// Note
+// You'll need to remove all non-alphanumeric characters (punctuation, spaces
+// and symbols) and turn everything lower case in order to check for palindromes.
+//
+// We'll pass strings with varying formats, such as "racecar", "RaceCar", and
+// "race CAR" among others.
+//
+// We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2",
+// and "2_A3*3#A2".
+
 function palindrome(str) {
-  var strCopy=str=str.toLowerCase();
-//  console.log(str);
-//  console.log(strCopy);
-  var re = /[a-z0-9]+/g;
-  var re2 = /\S/g;
-  //  /[a-z0-9]+.*/g;
-  var strArr= strCopy.match(re);
-//  console.log(strArr);
-  str=strCopy=strArr.join('');
-  console.log(strCopy);
-  strArr = strCopy.match(re2);
-//  console.log(strArr);
-  strArr=strArr.reverse();
-  strCopy = strArr.join('');
-  console.log(strCopy);
-  if (str === strCopy) {
-    console.log("TRUE");
-  }else { 
-    console.log("FALSE");
-  }
-  return true;
+  var pal = [];
+  str.toLowerCase().split('').reduce((a,b)=>{
+    (b.match(/[a-z0-9]/)) ? pal.push(b) :  b
+    return b
+  },[])
+   return (pal.join('') == pal.reverse().join(''))
 }
 
-palindrome("This is A 123 *&^#! tesT FOR TOlOWeR");
-palindrome("Race car");
-palindrome("A man, a plan, a canal. Panama");
+console.log(palindrome("eye"));// should return a boolean.
+console.log(palindrome("eye"));// should return true.
+console.log(palindrome("_eye"));// should return true.
+console.log(palindrome("race car"));// should return true.
+console.log(palindrome("not a palindrome"));// should return false.
+console.log(palindrome("A man, a plan, a canal. Panama"));// should return true.
+console.log(palindrome("never odd or even"));// should return true.
+console.log(palindrome("nope"));// should return false.
+console.log(palindrome("almostomla"));// should return false.
+console.log(palindrome("My age is 0, 0 si ega ym."));// should return true.
+console.log(palindrome("1 eye for of 1 eye."));// should return false.
+console.log(palindrome("0_0 (: /-\ :) 0-0"));// should return true.
+console.log(palindrome("five|\_/|four"));// should return false.
