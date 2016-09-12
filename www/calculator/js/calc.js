56023e3a37c903e99170dd5e58dfd28cdf9cbfa7
Paul Hosler <pahosler@gmail.com>
2016-09-12 16:42:36 -0500

    wip

--- www/calculator/js/calc.js
+++ www/calculator/js/calc.js
@@ -16,28 +16,6 @@ $( document ).ready( function() {
         var $divide = $el.find( ".divide" );
         var $multiply = $el.find( ".multiply" );
         var $equals = $el.find( ".equals" );
-        var funcKey = {
-            offPlus: '-2px -70px',
-            offMinus: '-71px -70px',
-            offMultiply: '-142px -70px',
-            offDivide: '-214px -70px',
-            offEquals: '-287px -70px',
-            offMemAdd: '-430px -70px',
-            offMemMinus: '-501px -70px',
-            offMemRecall: '-573px -70px',
-            offMemClear: '-645px -70px',
-            offClear: '-358px -70px',
-            onPlus: '-2px 0',
-            onMinus: '-71px 0',
-            onMultiplay: '-142px 0',
-            onDivide: '-214px 0',
-            onEquals: '-287px 0',
-            onClear: '-358px 0',
-            onMemAdd: '-430px 0',
-            onMemMinus: '-501px 0',
-            onMemRecall: '-573px 0',
-            onClear: '-358px 0'
-        };
 
         // number keys
         var $keydot = $el.find( ".dot" );
@@ -51,57 +29,28 @@ $( document ).ready( function() {
         var $key7 = $el.find( ".seven" );
         var $key8 = $el.find( ".eight" );
         var $key9 = $el.find( ".nine" );
+
         var slot = [ '#nixie0', '#nixie1', '#nixie2', '#nixie3', '#nixie4', '#nixie5', '#nixie6', '#nixie7' ];
         var nixie = [ '0 0', '-40px 0', '-80px 0', '-120px 0', '-160px 0', '-200px 0', '-240px 0', '-280px 0', '-320px 0', '-360px 0', '-400px 0', '-440px 0', '-478px 0', '-516px 0' ];
+
         var buffer = []; // stores key presses
         var values = []; // stores values to be evaluated
-        // flags +,-,*,/,clear
-        // flags reset to zero and set flag when key is pressed
-        var FLAG = {
-            PLUS: 1,
-            MINUS: 2,
-            MULTIPLY: 4,
-            DIVIDE: 8,
-            EQUALS: 16,
-            DOT: 32,
-            CLEAR: 64
-        }; // binary 000000
-
-        var clrAllMask = FLAG.CLEAR | FLAG.DOT | FLAG.EQUALS | FLAG.DIVIDE | FLAG.MULTIPLY | FLAG.MINUS | FLAG.PLUS;
-        clrMask = FLAG.CLEAR | FLAG.DOT & FLAG.EQUALS & FLAG.DIVIDE & FLAG.MULTIPLY & FLAG.MINUS & FLAG.PLUS;
-        console.log( "clrAllMask", clrAllMask.toString( 2 ) );
-        console.log( "clrMask", clrMask.toString( 2 ) );
-        var opFlags = [ 0, 0, 0, 0, 0, 0, 0 ];
-        var mem = 0;
-        var answer = false;
-        var funkeys = {
-            plus: 0,
-            minus: 1,
-            mult: 2,
-            divide: 3,
-            equals: 4,
-            dot: 5,
-            clr: 6
-        }
 
         // bind events
         $clear.on( 'click', function() {
-            doClear();
 
         } );
         $memplus.on( 'click', function() {
-            doMem( '+' );
+            memory( '+' );
         } );
         $memminus.on( 'click', function() {
-            doMem( '-' );
+            memory( '-' );
         } );
         $memrecall.on( 'click', function() {
-            opFlags[ funkeys.clr ] = 0;
-            doClear();
-            doMem( 'r' )
+            memory( 'r' )
         } );
         $memclear.on( 'click', function() {
-            mem = 0;
+            memory( 'c' );
         } );
         $plus.on( 'click', function() {
             opFlags[ funkeys.dot ] = 0;
@@ -235,43 +184,11 @@ $( document ).ready( function() {
         _render();
 
         function _render() {
-            // Do Something here!
-            // console.log( values );
             console.log( "buffer render", buffer );
             nixiesOff();
             for ( var i = 0; i < buffer.length; ++i ) {
                 $( slot[ i ] ).css( "background-position", nixie[ buffer[ i ] ] );
             }
-        }
-
-        function nixiesOnOff() {
-            var pos = 0;
-            var aNixie = 13;
-            var blankNixie = nixie[ 11 ];
-            var dPos = 1; //delta pos to change direction that nixies engage
-            var dNix = 1; // delta nixie to change count direction
-            function calcError() {
-                for ( var i = 0; i < slot.length; ++i ) {
-                    $( slot[ i ] ).css( "background-position", blankNixie );
-                }
-                $( slot[ pos ] ).css( "background-position", nixie[ aNixie ] );
-                pos += dPos;
-                // aNixie += dNix;
-                if ( pos > 6 || pos < 1 ) {
-                    dPos = -dPos
-                }
-                if ( aNixie > 12 || aNixie < 1 ) {
-                    dNix = -dNix
-                }
-            }
-
-            function clearError() {
-                clearInterval( runError );
-            }
-            var runError = setInterval( function() {
-                calcError()
-            }, 300 );
-
         }
 
         function nixiesOff() {
@@ -339,7 +256,9 @@ $( document ).ready( function() {
             _render();
         }
 
-        function doMem( op ) {
+        function flag
+
+        function memory( op ) {
             mem = parseInt( mem );
             var memVal = mem;
             if ( buffer.length > 0 ) {
@@ -356,6 +275,9 @@ $( document ).ready( function() {
                 mem += memVal;
             } else if ( op === '-' && buffer.length > 0 ) {
                 mem -= memVal;
+            } else if ( op === 'c' ) {
+                mem = 0;
+                return;
             } else {
                 buffer = [];
                 // values.push( mem );
@@ -370,49 +292,11 @@ $( document ).ready( function() {
                         buffer.splice( i, 1, 13 );
                     }
                 } );
-                // answer = true;
                 _render();
             }
         }
 
         function doClear() {
-            // var flags = {
-            //     plus: function( val ) {
-            //         opFlags[ funkeys.plus ] = val
-            //     },
-            //     minus: function( val ) {
-            //         opFlags[ funkeys.minus ] = val
-            //     },
-            //     mult: function( val ) {
-            //         opFlags[ funkeys.mult ] = val
-            //     },
-            //     divide: function( val ) {
-            //         opFlags[ funkeys.divide ] = val
-            //     },
-            //     dot: function( val ) {
-            //         opFlags[ funkeys.dot ] = val
-            //     },
-            //     clr: function( val ) {
-            //         opFlags[ funkeys.clr ] = val
-            //     }
-            // };
-            var buffers = {
-                buffer: function() {
-                    buffer = []
-                },
-                values: function() {
-                    values = []
-                }
-            };
-            // var setflag = function( flag, val ) {
-            //     flags.flag;
-            //     console.log( opFlags[ funkeys.flag ] );
-            //
-            // }
-
-            // return {
-            //     setflag: setflag
-            // }
 
             if ( opFlags[ funkeys.clr ] == 0 ) {
                 // reset function
@@ -439,13 +323,9 @@ $( document ).ready( function() {
             }
         }
         return {
-            nixiesOnOff: nixiesOnOff,
             nixiesOff: nixiesOff
         }
 
-        // nixiesOnOff.runError();
-
     } )();
     calculator.nixiesOff();
-    // calculator.nixiesOnOff.runError;
 } );
