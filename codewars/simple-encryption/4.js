5dd082fe985140af1d33f2cc5dea00f3b5dbc597
Paul Hosler <pahosler@gmail.com>
2017-03-23 01:21:15 -0500

    update

---
+++ codewars/simple-encryption/4.js
@@ -0,0 +1,99 @@
+// The crypting-regions are these 3 lines from your keyboard:
+// 1. "qwertyuiop"
+// 2. "asdfghjkl"
+// 3. "zxcvbnm,."
+//
+// If a char of the string is not in any of these regions, take the char direct in the output.
+// If a char of the string is in one of these regions: Move it by the part of the key in the
+// region and take this char at the position from the region.
+// If the movement is over the length of the region, continue at the beginning.
+// The encrypted char must have the same case like the decrypted char!
+// So for upperCase-chars the regions are the same, but with pressed "SHIFT"!
+//
+// The Encryption-Key is an integer number from 000 to 999. E.g.: 127
+//
+// The first digit of the key (e.g. 1) is the movement for the first line.
+// The second digit of the key (e.g. 2) is the movement for the second line.
+// The third digit of the key (e.g. 7) is the movement for the third line.
+//
+// (Consider that the key is an integer! When you got a 0 this would mean 000. A 1 would mean 001. And so on.)
+// You do not need to do any prechecks. The strings will always be not null and will always have a length > 0. You do not have to throw any exceptions.
+//
+// An Example:
+//
+// Encrypt "Ball" with key 134
+// 1. "B" is in the third region line. Move per 4 places in the region. -> ">" (Also "upperCase"!)
+// 2. "a" is in the second region line. Move per 3 places in the region. -> "f"
+// 3. "l" is in the second region line. Move per 3 places in the region. -> "d"
+// 4. "l" is in the second region line. Move per 3 places in the region. -> "d"
+// --> Output would be ">fdd"
+
+function encrypt(text, key) {
+  return simple(text,key);
+}
+
+function decrypt(text, key) {
+  return simple(text,key);
+}
+
+function simple(text,key) {
+  var crypt = new cipher();
+  var letters = text.split('');
+  var encryptKey = crypt.keys(key);
+  var letter = text;
+
+  console.log('encryptKey',encryptKey,encryptKey.length);
+  //var decryptKey = encryptKey;
+  var region = 1;// getLetterRow();
+  /*
+      you could reverse the cipher this way
+
+      var revRow = crypt.row.lowerEnc[letterRow].split('').reverse().join('');
+
+      although I think using an array will be faster<?>
+  */
+
+  var letterNum = crypt.row.lowerEnc[region].indexOf(letter);
+  var cryptLetterNum = (letterNum+encryptKey[0])%crypt.row.lowerEnc[region].length;
+  var cryptLetter = crypt.row.lowerEnc[region].substring(cryptLetterNum,cryptLetterNum+1);
+  console.log(letter,letterNum,cryptLetter,cryptLetterNum);
+  console.log('<','<'.charCodeAt(0));
+  console.log('>','>'.charCodeAt(0));
+  console.log(',',','.charCodeAt(0));
+  console.log('.','.'.charCodeAt(0));
+  console.log('a','a'.charCodeAt(0));
+  console.log('z','z'.charCodeAt(0));
+
+}
+
+
+var cipher = function () {
+
+    var row = {
+      lowerEnc: ["qwertyuiop", "asdfghjkl", "zxcvbnm,."],
+      upperEnc: ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM<>"],
+      lowerDec: ["poiuytrewq","lkjhgfdsa",".,mnbvcxz"],
+      upperDec: ["POIUYTREWQ","LKJHGFDSA","><MNBVCXZ"]
+    };
+    function getLetterRow(letter){
+
+    }
+    function isUpper(letter){
+
+    }
+    function keys(key){
+      var keyArr = key.toString().split('');
+      if (keyArr.length < 3) {
+        for (var i = keyArr.length; i < 3; ++i){
+          keyArr.unshift('0');
+        }
+      }
+      return keyArr;
+    }
+  return {
+    row:row,
+    keys:keys
+
+  };
+  };
+encrypt('l',713);
